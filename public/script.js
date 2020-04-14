77c5e3c93c6a477bad5ce989460943558f79dfcb
Flurach <flurach.dev@gmail.com>
2020-04-14 16:50:08 +0300

    Fixed links

--- public/script.js
+++ public/script.js
@@ -4,21 +4,7 @@ document.querySelectorAll('#links li, .project h4').forEach(el => {
 		window.open(el.getAttribute('href'), '_blank')
 })
 
-// Set nav a#contact
-let rotateAngle = 0
-document.querySelector('a[href="#header"]').onclick = function() {
-	window.scrollTo(0, 0)
-	const links = document.querySelector('#links')
-
-	rotateAngle += 360;
-	links.style.filter = `rotateZ(${rotateAngle}deg)`
-	setTimeout(
-		() => links.style.transform = `rotateZ(${rotateAngle}deg)`,
-		250
-	)
-}
-
-// Set image displayer
+// Set displayables
 document.querySelectorAll('img.displayable').forEach(image =>
 	image.onclick = () =>
 		window.open(image.getAttribute('src'), '_blank')
