9443b88b45f860032151f30a25fc659ea9686a6b
Flurach <flurach.dev@gmail.com>
2020-04-14 04:00:02 +0300

    Removed image displayer
    It was unnecessary actually. Everyone can do it with a little of JS. And
    doesn't actually show what I can do :/

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
-// Set contact links
-document.querySelectorAll('#links li').forEach(li => {
-	li.onclick = () =>
-		window.location.href = li.getAttribute('href')
+// Set links
+document.querySelectorAll('#links li, .project h4').forEach(el => {
+	el.onclick = () =>
+		window.open(el.getAttribute('href'), '_blank')
 })
 
 // Set nav a#contact
@@ -19,23 +19,7 @@ document.querySelector('a[href="#header"]').onclick = function() {
 }
 
 // Set image displayer
-let img_displayer = document.getElementById('img-displayer');
-let img_displayer_img = img_displayer.getElementsByTagName('img')[0];
-let displayable_images = document.querySelectorAll('img.displayable');
-
-displayable_images.forEach(image => {
-	image.onclick = () => {
-		let url = image.getAttribute('src');
-		img_displayer_img.setAttribute('src', url);
-		img_displayer.style.left = '0px';
-	}
-})
-
-img_displayer.onclick = () =>
-	img_displayer.style.left = '100%';
-
-// Set project links
-document.querySelectorAll('.project h4').forEach(link =>
-	link.onclick = () =>
-		window.location.href = link.getAttribute('href')
+document.querySelectorAll('img.displayable').forEach(image =>
+	image.onclick = () =>
+		window.open(image.getAttribute('src'), '_blank')
 )
