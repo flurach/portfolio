15fdc71c8dd9842f9b4988743499c15f64c2295d
Flurach <flurach.dev@gmail.com>
2020-04-16 07:16:46 +0300

    Re-designed header

--- public/script.js
+++ public/script.js
@@ -9,12 +9,3 @@ document.querySelectorAll('img.displayable').forEach(image =>
 	image.onclick = () =>
 		window.open(image.getAttribute('src'), '_blank')
 )
-
-// Set nav btn
-document.querySelector('nav a:last-child').onclick = () => {
-	window.scrollTo(0, 0)
-
-	const links = document.getElementById('links')
-	links.style.scale = '1.2'
-	setTimeout(() => links.style.scale = '1', 250)
-}
