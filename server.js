24e7a551f48bc90f0ecf8421aa9c66350fffec27
Flurach <flurach.dev@gmail.com>
2020-04-27 10:00:45 +0300

    Added print to listen

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ app.use(express.static('./public'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
 
 
-app.listen(process.env.PORT || 8080)
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
